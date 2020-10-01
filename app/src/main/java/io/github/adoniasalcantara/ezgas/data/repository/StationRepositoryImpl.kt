9680838870331cd86c863794e417402c18ba38a3
AdoniasAlcantara <adoniasasneto@gmail.com>
2020-10-01 20:19:18 -0300

    Add StationRepositoryImpl

--- app/src/main/java/io/github/adoniasalcantara/ezgas/data/repository/StationRepositoryImpl.kt
+++ app/src/main/java/io/github/adoniasalcantara/ezgas/data/repository/StationRepositoryImpl.kt
@@ -1,10 +1,21 @@
 package io.github.adoniasalcantara.ezgas.data.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import io.github.adoniasalcantara.ezgas.data.api.ApiFactory
 import kotlinx.coroutines.flow.Flow
 
-class StationRepositoryImpl : StationRepository {
+class StationRepositoryImpl(apiFactory: ApiFactory) : StationRepository {
+
+    private val api by lazy { apiFactory.stationApi() }
 
     override fun searchNearby(query: NearbyQuery): Flow<NearbyResult> {
-        TODO("Not yet implemented")
+        val config = PagingConfig(
+            pageSize = 10,
+            initialLoadSize = 10,
+            enablePlaceholders = false,
+        )
+
+        return Pager(config) { NearbySource(api, query) }.flow
     }
 }
